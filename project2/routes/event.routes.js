6f188a2209bbf3ac09d6a505f3eec8b55bfd4152
mauro404 <mau.mansano@gmail.com>
2022-10-08 17:57:43 +0200

    created user and event model + basic route structure

---
+++ project2/routes/event.routes.js
@@ -0,0 +1,28 @@
+
+const router = require('express').Router();
+
+
+//READ: List all events
+//get /events
+
+
+//CREATE: display form
+// get /events/create
+
+//CREATE: process form
+// post /events/create
+
+
+//READ: Event Details
+//get events/:eventId
+
+
+//UPDATE: display form
+// get /events/:eventId/update
+
+//UPDATE: process form
+// post /events/:eventId/update
+
+//DELETE:
+// post /events/:eventId/delete
+
---
+++ project2/routes/user.routes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+
+//READ: display the profile of the User
+// get /user/profile
