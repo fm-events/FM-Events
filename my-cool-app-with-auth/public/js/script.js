1caf5e1f572adcd14a52c3633e1aade0846b7659
Farzin khaledi <Khaledifarzin1@gmail.com>
2022-10-08 13:29:23 +0100

    first commit

---
+++ my-cool-app-with-auth/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("my-cool-app-with-auth JS imported successfully!");
+  },
+  false
+);
---
+++ my-cool-app-with-auth/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/my-cool-app-with-auth";
+
+module.exports = MONGO_URI;
